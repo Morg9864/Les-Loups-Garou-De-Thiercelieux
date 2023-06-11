85519e20d99339f7e16c70ee593a57d18f088ae1
Morg9864 <morgan.phemba@student.uliege.be>
2023-06-11 16:11:18 +0200

    Ajout des boutons

--- source/view/SimulatorMainGUI.java
+++ source/view/SimulatorMainGUI.java
@@ -1,7 +1,12 @@
 package source.view;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.io.File;
+import java.io.IOException;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
@@ -23,6 +28,27 @@ public class SimulatorMainGUI extends JFrame{
         JLabel background = new JLabel(new ImageIcon("images/background.jpg"));
         background.setBounds(0, -50, 1760, 1080);
         layeredPane.add(background, Integer.valueOf(0));
+
+        // Création de 2 boutons
+        JButton generateRoleButton = new JButton("Générer les rôles");
+        generateRoleButton.setBounds(309, 654, 462, 139);
+        layeredPane.add(generateRoleButton, Integer.valueOf(1));
+
+        JButton loadRoleButton = new JButton("Charger les rôles");
+        loadRoleButton.setBounds(975, 654, 462, 139);
+        layeredPane.add(loadRoleButton, Integer.valueOf(1));
+        
+        try {
+            Font font = Font.createFont(Font.TRUETYPE_FONT, new File("font/TheMacabre.otf"));
+            generateRoleButton.setFont(font.deriveFont(60f));
+            loadRoleButton.setFont(font.deriveFont(60f));
+        } catch (FontFormatException e) {
+            System.out.println("Erreur lors du chargement de la police");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Fichier de police introuvable");
+            e.printStackTrace();
+        }
 
         // Affichage de la fenêtre
         setLocationRelativeTo(null);
