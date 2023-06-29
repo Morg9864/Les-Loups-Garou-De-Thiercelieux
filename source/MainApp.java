ba6ae209afcff3aa1a343070aaa5cfa4d85e8827
Morg9864 <morgan.phemba@student.uliege.be>
2023-06-29 03:50:35 +0200

    Lancement de l'appli depuis AppView()

--- source/MainApp.java
+++ source/MainApp.java
@@ -1,9 +1,9 @@
 package source;
 
-import source.view.SimulatorMainGUI;
+import source.view.AppView;
 
 public class MainApp {
     public static void main(String[] args) {
-        new SimulatorMainGUI();
+        new AppView();
     }
 }
