5b42e781d80c0e64e4357d0704d98858015197fb
Morg9864 <morgan.phemba@student.uliege.be>
2023-06-30 01:30:27 +0200

    Fichier contenant des constantes

---
+++ source/view/Const.java
@@ -0,0 +1,15 @@
+package source.view;
+
+public interface Const {
+    public static final String MAIN_BACKGROUNG = "images/background.jpg";
+    public static final String BACK_ARROW = "images/back.png";
+    public static final String LOAD_BACKGROUND = "images/load.jpg";
+    public static final String GENERATE_BACKGROUND = "images/generate.jpg";
+    public static final String ROLES_BACKGROUND = "images/roles.jpg";
+    public static final String THE_MACABRE_FONT = "font/TheMacabre.otf";
+    public static final String ROLES_FILE = "roles.txt";
+    public static final int WINDOW_WIDTH = 1760;
+    public static final int WINDOW_HEIGHT = 1080;
+    public static final Integer BACKGROUND_CONSTRAINT = Integer.valueOf(0);
+    public static final Integer FOREGROUND_CONSTRAINT = Integer.valueOf(1);
+}
