27716a7a29d08a8dfacca53ed6b4c036d094159f
Morg9864 <morgan.phemba@student.uliege.be>
2023-06-30 18:10:06 +0200

    Gestion de la sauvegarde et de l'affichage des boutons d'extension

--- source/controller/Controls.java
+++ source/controller/Controls.java
@@ -1,12 +1,22 @@
 package source.controller;
 
 import java.awt.BorderLayout;
+import java.io.IOException;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
+import source.model.Simulator;
 import source.view.AppView;
+import source.view.Const;
+import source.view.GeneratingRoles;
+import source.view.SavePopUp;
+import source.view.SettingUpGenerator;
 
-public class Controls {
+public class Controls implements Const{
     private Controls() {
     }
 
@@ -18,4 +28,27 @@ public class Controls {
         appView.getContentPane().revalidate();
     }
     
+    public static void saveConfiguration(AppView appView, Simulator simulator){
+        try {
+            simulator.save(ROLES_FILE);
+            new SavePopUp(appView);
+        } catch (IOException e) {
+            System.out.println("Erreur lors de la sauvegarde de la configuration");
+            e.printStackTrace();
+        }
+    }
+
+    public static void getExtension(AppView appView, JLayeredPane layeredPane, JTextArea textArea,
+            JComboBox<Integer> nbPlayersList, JButton validateButton) {
+        int nbPlayersSelected = (int) nbPlayersList.getSelectedItem();
+        if(nbPlayersSelected < 43){
+            textArea.setText("Choix extension :");
+            textArea.setBounds(735, 450, 350, 70);
+            layeredPane.remove(nbPlayersList);
+            layeredPane.remove(validateButton);
+            SettingUpGenerator.extension(appView, layeredPane, nbPlayersSelected);
+        } else {
+            Controls.update(appView, new GeneratingRoles(appView, nbPlayersSelected, true, true));
+        }
+    }
 }
