0ba6b22ab2a68006a0593b77daba3785fe118710
Morg9864 <morgan.phemba@student.uliege.be>
2023-06-30 18:12:44 +0200

    Interface de sauvegarde (pop-up)

---
+++ source/view/SavePopUp.java
@@ -0,0 +1,23 @@
+package source.view;
+
+import java.awt.Font;
+
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+
+public class SavePopUp extends JDialog implements Const{
+    public SavePopUp(AppView appView) {
+        super(appView, "Configuration sauvegardée", true);
+        setSize(500, 200);
+        JLabel label = new JLabel("La configuration a été sauvegardée avec succès !");
+        label.setFont(new Font("Arial", Font.BOLD, 17));
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setVerticalAlignment(JLabel.CENTER);
+        add(label);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setModal(true);
+        setVisible(true);
+    }
+}
